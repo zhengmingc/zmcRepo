2d1dbb6fe733352eac037017c064f099acda466e
zhengmingc <chuzhengming@gmail.com>
2013-10-17 16:28:39 -0700

    add point class

---
+++ WCCon/WCCon/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCCon
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0},{1}]", this.X, this.Y);
+
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static Point operator ++(Point p1)
+        {
+            return new Point(p1.X + 1, p1.Y + 1);
+        }
+
+        public static Point operator --(Point p1)
+        {
+            return new Point(p1.X - 1, p1.Y - 1);
+        }
+    }
+}
