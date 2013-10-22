6f2efdccea004304352a82c9cfacec5b714dc30d
zhengmingc <chuzhengming@gmail.com>
2013-10-22 10:14:54 -0700

    Add partial methods

---
+++ WCCon/WCCon/CarLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCCon
+{
+    partial class CarLocator
+    {
+        public bool CarAvailableInZipCode(string zipCode)
+        {
+            OnZipCodeLookup(zipCode);
+            return true;
+        }
+
+        partial void OnZipCodeLookup(string make);
+    }
+}
