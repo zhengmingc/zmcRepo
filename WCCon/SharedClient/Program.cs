df01dede6ab3eb70fce3618065b69ad533f5d1d9
zhengmingc <chuzhengming@gmail.com>
2013-11-06 15:57:16 -0800

    Add sharedClient project

---
+++ WCCon/SharedClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CarLibrary;
+
+namespace SharedClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shared Assembly");
+            SportsCar sc = new SportsCar("BMW",300,20);
+            sc.TurboBoost();
+            Console.ReadLine();
+
+        }
+    }
+}
