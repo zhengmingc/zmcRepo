94604bf8e7ac6e9a9d39ea282dab2cf1bccfd293
zhengmingc <chuzhengming@gmail.com>
2013-11-19 16:19:27 -0800

    Add custom attributes

---
+++ WCCon/AttributedCarLibrary/Motorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{   [Serializable]
+    [VehicleDescription(Description = "My rocking Harley")]
+    public class Motorcycle
+    {
+     [VehicleDescription(Description = "My rocking Harley")]
+     public void PlayMusic(bool on)
+     {}
+    }
+
+    [VehicleDescription("It is winnebago")]
+    public class Winnebago
+    {
+
+    }
+}
---
+++ WCCon/AttributedCarLibrary/VehicleDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{  
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method)]
+    public sealed class VehicleDescriptionAttribute:System.Attribute
+    {
+        public string Description { get; set; }
+
+        public VehicleDescriptionAttribute(string vehicalDescription)
+        {
+            Description = vehicalDescription;
+        }
+
+        public VehicleDescriptionAttribute()
+        {
+            
+        }
+    }
+}
