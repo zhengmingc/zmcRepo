44b3c9a9023b451a892fe04f76622c9198499735
zhengmingc <chuzhengming@gmail.com>
2013-11-22 16:04:07 -0800

    Add extendable app

---
+++ WCCon/VBSnapIn/VbSnapIn.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Windows.Forms
+Imports CommonSnappableTypes
+
+<CompanyInfo(CompanyName:="VB Wenwen", CompanyUrl:="wenwenvb.com")>
+Public Class VbSnapIn
+    Implements IAppFunctionality
+
+    Public Sub DoIt() Implements CommonSnappableTypes.IAppFunctionality.DoIt
+        MessageBox.Show("You have just used vb application")
+    End Sub
+
+End Class
