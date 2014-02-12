6e583ec1c3fcf1a44018381f6dbf848fe28c3cc0
zhengmingc <chuzhengming@gmail.com>
2014-02-11 22:51:36 -0800

    Commit whatever in home pc

--- WCCon/CustomAppDomains/Program.cs
+++ WCCon/CustomAppDomains/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,6 +13,12 @@ namespace CustomAppDomains
         static void Main(string[] args)
         {
             AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            defaultAD.ProcessExit += (o, s) =>
+                {
+                    Console.WriteLine("Default AD unloaded !!!!!!!");
+                };
+
             ListAllAssembliesInAppDomain(defaultAD);
             MakeNewAppDomain();
             Console.ReadLine();
@@ -20,7 +27,18 @@ namespace CustomAppDomains
         private static void MakeNewAppDomain()
         {
             AppDomain newAD = AppDomain.CreateDomain("SecondAppDomain");
+
+            try
+            {
+                newAD.Load("CarLibrary");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
             ListAllAssembliesInAppDomain(newAD);
+
+            AppDomain.Unload(newAD);
         }
 
         private static void ListAllAssembliesInAppDomain(AppDomain ad)
@@ -33,6 +51,8 @@ namespace CustomAppDomains
 
             foreach (var a in loadedAssemblies)
             {
+                Console.WriteLine("Name ==>> {0}", a.GetName().Name);
+                Console.WriteLine("Version ==>> {0}", a.GetName().Version);
                 
             }
         }
